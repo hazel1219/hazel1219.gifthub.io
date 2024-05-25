986a943b8d54766edbf1b4977589f5f49323a355
hazel1219 <delossantoshazel19@gmail.com>
2024-05-25 18:16:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+Const btnLike1 = document.getElementByI(“btnLike1”)
+Const countLike1 = documebt.getElementById(“countLike1”)
+
+Function clickLike1(){
+Let totalLikes1 =countLike1.value + 1. value) + 1
+countLike1. textContent = totalLikes. toString()
+
+}
+
